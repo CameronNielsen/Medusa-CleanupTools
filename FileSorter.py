1ac65783621addb7981bd5a6209c51c0286a702a
CameronNielsen <32140147+CameronNielsen@users.noreply.github.com>
2018-04-30 17:13:57 -0500

    Version 1.0
    Revised after some helpful tips from my coworker Henry Borcher. Now much more clean with its throughput and hopefully more transparent. Hopefully it won't crash now.

--- FileSorter.py
+++ FileSorter.py
@@ -1,27 +1,32 @@
 import glob
 import shutil
+import os
 
-fileext = input("Enter file extension of the files you want to move (without the dot): ")
-origin = input("Enter full path of the directory files to be moved are located in (ending with /): ")
-dest = input("Enter full path of the directory files are to be sorted into (ending with /): ")
-
+# fileext = input("Enter file extension of the files you want to move (without the dot): ")
+# origin = input("Enter full path of the directory files to be moved are located in (ending with /): ")
+# dest = input("Enter full path of the directory files are to be sorted into (ending with /): ")
+fileext = "txt"
+origin = "./Test/"
+dest = "./Test/"
 
 files = glob.glob(origin + '*.' + fileext)
 
 for file in files:
-    file_name = file.split("/")[-1]
-    file_name = file_name.split("\\")[-1]
-    print(file_name, "1")
-    file_title = file_name.split(".")[0]
-    print(file_title, "2")
-    id_number = file_title.split("_")[0]
-    print(id_number, "3")
-    direct = glob.glob((dest + '%s/') % id_number)[0]
-    print(direct)
-    subdir = glob.glob(direct + '0**/')
-    print(subdir)
-    for sub_path in subdir:
-        sub_path = sub_path.replace("\\", "/")
-        shutil.copy2((origin + file_name), (sub_path + "PDI/ObjectJPG/" + file_name))
-
-#To do: Add comments, make input/output less brittle, clean out ALL back slashes, add error messages for files not present, fix whatever makes it crash with large numbers of big files
+    file = os.path.normpath(file)
+    file_name = os.path.basename(file)
+    id_number = file_name.split("_")[0]
+    dest_dir_loc = os.path.join(dest, id_number)
+    if os.path.exists(dest_dir_loc):
+        subdir = glob.glob(dest_dir_loc + '/0**/')
+        for sub_path in subdir:
+            sub_path = os.path.normpath(sub_path)
+            origin_path = os.path.join(origin + file_name)
+            dest_path = os.path.join(sub_path + "/PDI/ObjectJPG/" + file_name)
+            shutil.copy2(origin_path, dest_path)
+    else:
+        print(dest_dir_loc, "does not exist!")
+# To do: Add comments, make input/output less brittle, clean out ALL back slashes, add error messages for files not
+# present, fix whatever makes it crash with large numbers of big files
+# New To Do after talking to Henry: use os.path for all cleanup of addresses, etc. Error handling for if no files.
+# Use debugger to figure out what's going wrong!
+# Update 4/30 --I think it's done?! Except for comments and some stress testing?
