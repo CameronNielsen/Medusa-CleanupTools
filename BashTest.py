7d0daad37cf1a8531969a327114a441067ed8339
CameronNielsen <ccniels2@illinois.edu>
2018-05-08 00:12:02 -0500

    Some advice from Robbie

--- BashTest.py
+++ BashTest.py
@@ -3,7 +3,9 @@ supp_id = "fjowm"
 
 import subprocess
 command = ['rsync', '-r', '--progress', "./TestZone/root/1114/2565/.", ('./' + coll_id + '-' + supp_id)]
-output, err = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()
-print(output)
+output, err = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).communicate()
+# print(output)
 
 # subprocess.run(['rsync', '-r', '--progress', "./TestZone/root/1114/2565/.", ('./' + coll_id + '-' + supp_id)])
+
+# output = subprocess.call(command), output.wait
