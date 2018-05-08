9e45325584073aae9a2429dbfbe25c7870756f19
CameronNielsen <32140147+CameronNielsen@users.noreply.github.com>
2018-05-07 23:27:13 -0500

    Still needs the Bash

--- SafeMassCopy.py
+++ SafeMassCopy.py
@@ -5,27 +5,38 @@ filein = open('addresses.txt', 'r')
 test_strings = filein.readlines()
 filein.close()
 
-for raw_input in test_strings:
-    if "    " in raw_input:
-        both_items = raw_input.split("  ")
-        raw_source = both_items[0]
-        supp_id = both_items[1]
-        print(raw_source, supp_id)
-    else:
-        raw_source = raw_input
-        print(raw_source)
-
-    if "root" in raw_source:
-        directories_list = raw_source.split("\\")
+# This function finds the collection ID number from the Medusa file system, which is always immediately under the root directory.
+def find_coll_id(source_addr):
+    if "root" in source_addr:
+        directories_list = source_addr.split("/")
         root_index = directories_list.index("root")
-        collection_ID = directories_list[root_index + 1]
-        print(collection_ID)
+        collection_id = directories_list[root_index + 1]
+        return collection_id
     else:
         print("You're using the wrong script!")
 
+# def run_rsync(args):
+#     import subprocess
+#
+
+for raw_input in test_strings:
+    raw_input = raw_input.replace("\\", "/")
+    if "\t" in raw_input:
+        both_items = raw_input.split("\t")
+        raw_source = both_items[0]
+        supp_id = both_items[1]
+        coll_id = find_coll_id(raw_source)
+        print(coll_id, supp_id)
 
 
+    elif "    " in raw_input:
+        both_items = raw_input.split("    ")
+        raw_source = both_items[0]
+        supp_id = both_items[1]
+        coll_id = find_coll_id(raw_source)
+        print(coll_id, supp_id)
 
+    else:
+        coll_id = find_coll_id(raw_input)
+        print(coll_id)
 
-
-# subprocess.run()
