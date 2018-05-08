3b238f5be375dbd1d6b215663b2279e93c544355
CameronNielsen <32140147+CameronNielsen@users.noreply.github.com>
2018-05-07 23:34:10 -0500

    I don't understand this at all

---
+++ BashTest.py
@@ -0,0 +1,9 @@
+coll_id = "aoimva"
+supp_id = "fjowm"
+
+import subprocess
+command = ['rsync', '-r', '--progress', "./TestZone/root/1114/2565/.", ('./' + coll_id + '-' + supp_id)]
+output, err = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()
+print(output)
+
+# subprocess.run(['rsync', '-r', '--progress', "./TestZone/root/1114/2565/.", ('./' + coll_id + '-' + supp_id)])
