d3881ce9a68c387f6dc2e5b77cd64ad1068605d0
CameronNielsen <32140147+CameronNielsen@users.noreply.github.com>
2018-05-10 16:28:16 -0500

    Update TreeReports.py

--- TreeReports.py
+++ TreeReports.py
@@ -7,13 +7,15 @@ for root, dirs, files in os.walk(working_dir):
         indent = '|  ' * (level)
         print('{}{}/'.format(indent, os.path.basename(root)))
         subindent = '|  ' * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
+        for file in files:
+                location = os.path.join(root, file)
+                if os.path.getsize(location) == 0:
+                        print('{}{}'.format(subindent, f))
                 # I'd like to replace this part with a loop that, instead of 
                 # listing each individual file, lists the first few of each file
                 # extension, then provides a count of how many of that type there are
                 # in the directory. This should be simple enough.
 
-duration = 1  # second
-freq = 440  # Hz
-os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
+# duration = 1  # second
+# freq = 440  # Hz
+# os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
