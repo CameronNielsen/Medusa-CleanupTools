1cb94d727f2225ada95369b984723f49a8691816
CameronNielsen <32140147+CameronNielsen@users.noreply.github.com>
2018-05-11 16:02:58 -0500

    Added descriptive comments

--- 0byteReporter.py
+++ 0byteReporter.py
@@ -1,23 +1,22 @@
-# Generates a tree list of all 0-byte files in a specified directory, arranges them in a tree
+# Generates a list of all 0-byte files in a specified directory, arranged in a tree.
 
 import os
 import datetime
 
-base_dir = input("Input full path of directory to evaluate: ")
+base_dir = input("Input full path of directory to evaluate (using forward slashes): ")
 report_name = os.path.basename(base_dir)    # Finds clean base name to identify printed report
 outfile = open(report_name + '_0byteReport.txt', 'w')
-print(str(datetime.datetime.now()), file=outfile)
-print("Base directory: " + base_dir, file=outfile)
+print(str(datetime.datetime.now()), file=outfile)  # Adds creation date information to report
+print("Base directory: " + base_dir, file=outfile)  # Adds scanned location information to report
 
 for root, dirs, files in os.walk(base_dir):
     level = root.replace(base_dir, '').count(os.sep)  # Counts the separators to determine how many levels down
-    # from the base directory we are
     indent = '  ' * level
     print('{}{}/'.format(indent, os.path.basename(root)), file=outfile)
     subindent = '   ' * (level + 1)
     for file in files:
         location = os.path.join(root, file)
-        if os.path.getsize(location) == 0:
+        if os.path.getsize(location) == 0:  # Detects and prints only empty files
             print('{}{}'.format(subindent, file), file=outfile)
 
 outfile.close()
