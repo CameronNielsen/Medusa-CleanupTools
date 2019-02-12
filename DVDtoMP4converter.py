62293787bb836aea67b15dace0d672b57337ac76
CameronNielsen <ccniels2@illinois.edu>
2019-02-12 16:39:02 -0600

    A bunch of working stuff

--- DVDtoMP4converter.py
+++ DVDtoMP4converter.py
@@ -1,22 +1,35 @@
+# Script to find DVD content extracted using our standard born-digital workflow, and then create derivative .MP4
+# files using HandBrakeCLI. The latter program is expected to be located at C:\Program Files\HandBrake\HandBrakeCLI.exe.
+# The derivative files thus created are placed in a directory called MigratedFiles parallel to the OriginalFiles
+# directory already existing.
+# By Cameron Nielsen, Feb. 12, 2019
+
 import os
 import glob
 import subprocess
-log = open(HandBrakeConversion.log, 'w')
 
-cwd = os.getcwd()
+working_dir = input("Enter the full path of the content directory of the collection with DVD videos to convert: ")
+# working_dir = os.getcwd()
 
-for item_level_dir in os.listdir(cwd):
-    item_level_path = os.path.join(cwd, item_level_dir)
-    search_path = os.path.normpath(item_level_path + os.sep + "**" + os.sep + "VIDEO_TS" + os.sep)
+for item_level_dir in os.listdir(working_dir):
+    item_level_path = os.path.join(working_dir, item_level_dir)
+    search_path = os.path.normpath(item_level_path + "/**/VIDEO_TS/")
     found_content_dirs = glob.glob(search_path, recursive=True)
     if len(found_content_dirs) == 0:
+        log = open("HandBrakeConversion.log", 'w')
         print("NOTE:", item_level_path, "does not contain correctly formatted DVD files!", '\n', file=log)
+        log.close()
     else:
         for content_dir in found_content_dirs:
+            log = open("HandBrakeConversion.log", 'w')
             content_dir = os.path.normpath(content_dir)
-            output_file_name = item_level_dir + "_deriv.mp4"
-            output_file_path = os.path.join(content_dir, output_file_name)
-            subprocess.run(["C:\Program Files\HandBrake\HandBrakeCLI.exe", "-i", content_dir, "-o", output_file_path, "-e", "x264", "-q", "20", "-B", "160"])
-            print("Converted video file created at", output_file_path, '\n', file=log)
-
-report.close()
+            content_dir_str = str(content_dir)
+            disk_level_dir = content_dir_str.split("OriginalFiles")[0]
+            migratedfiles_dir = os.path.join(disk_level_dir, "MigratedFiles/")
+            output_file_name = os.path.join(migratedfiles_dir + str(item_level_dir) + ".mp4")
+            migratedfiles_dir = os.path.normpath(migratedfiles_dir)
+            output_file_name = os.path.normpath(output_file_name)
+            os.mkdir(migratedfiles_dir)
+            subprocess.run(["C:\Program Files\HandBrake\HandBrakeCLI.exe", "-i", content_dir, "-o", output_file_name, "-e", "x264", "-q", "20", "-B", "160"])
+            print("Converted video file created at", output_file_name, '\n', file=log)
+            log.close()
--- ObjectJPGsorter.py
+++ ObjectJPGsorter.py
@@ -1,5 +1,7 @@
-# Sorts PDI photographs by serial number into their appropriate locations in the born-digital reformatting
-# output filesystem. Should work on Windows and Linux, but only tested on Windows so far.
+# Sorts PDI photographs by Object ID number (or anything as long as it is followed by an underscore) into their
+# appropriate locations in the born-digital reformatting output filesystem. Should work on Windows and Linux, but
+# only tested on Windows so far.
+# By Cameron Nielsen, Feb. 12, 2019
 
 
 import glob
@@ -7,22 +9,28 @@ import shutil
 import os
 
 fileext = input("Enter file extension of the files you want to move (without the dot): ")
-origin = input("Enter full path of the directory files to be moved are located in (ending with /): ")
-dest = input("Enter full path of the directory files are to be sorted into (ending with /): ")
+origin = input("Enter full path of the directory containing the files to be moved: ")
+dest = input("Enter full path of the directory files are to be sorted into: ")
 
-files = glob.glob(origin + '*.' + fileext)
+
+searchpath = os.path.join(origin + '/*.' + fileext)
+searchpath = os.path.normpath(searchpath)
+print(searchpath)
+files = glob.glob(str(searchpath))
 
 for file in files:
     file = os.path.normpath(file) 
     file_name = os.path.basename(file)
     id_number = file_name.split("_")[0]
-    dest_dir_loc = os.path.join(dest, id_number)
+    dest_dir_loc = os.path.join(dest + "/" + id_number)
+    dest_dir_loc = os.path.normpath(dest_dir_loc)
+    print(dest_dir_loc)
     if os.path.exists(dest_dir_loc):
         subdir = glob.glob(dest_dir_loc + '/0**/')
         for sub_path in subdir:
             sub_path = os.path.normpath(sub_path)
-            origin_path = os.path.join(origin + file_name)
-            dest_path = os.path.join(sub_path + "/PDI/ObjectJPG/" + file_name)
+            origin_path = os.path.normpath(os.path.join(origin + "/" + file_name))
+            dest_path = os.path.normpath(os.path.join(sub_path + "/PDI/ObjectJPG/" + file_name))
             shutil.copy2(origin_path, dest_path)
     else:
         print(dest_dir_loc, "does not exist!")
