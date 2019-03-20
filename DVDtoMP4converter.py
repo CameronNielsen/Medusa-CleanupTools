0c4d3220fa20005a6d881d1c80a4595f1d7bd3df
CameronNielsen <ccniels2@illinois.edu>
2019-03-20 16:14:59 -0500

    Fixed log file overwrite issue

--- DVDtoMP4converter.py
+++ DVDtoMP4converter.py
@@ -4,6 +4,8 @@
 # directory already existing.
 # The VIDEO_TS directory name is a part of the DVD specification, so it is hard-coded in as both the search term and the
 # file name for derivative outputs.
+# NOTE: The HandBrake settings used in this command have not been reviewed by anyone from AV Pres, I just picked them
+# out best I could as a "good enough" selection for the task at hand.
 
 # By Cameron Nielsen, Feb. 12, 2019
 
@@ -19,12 +21,11 @@ for item_level_dir in os.listdir(working_dir):
     search_path = os.path.normpath(item_level_path + "/**/VIDEO_TS/")
     found_content_dirs = glob.glob(search_path, recursive=True)
     if len(found_content_dirs) == 0:
-        log = open("HandBrakeConversion.log", 'w')
+        log = open("HandBrakeConversion.log", 'a')
         print("NOTE:", item_level_path, "does not contain correctly formatted DVD files!", '\n', file=log)
         log.close()
     else:
         for content_dir in found_content_dirs:
-            log = open("HandBrakeConversion.log", 'w')
             content_dir = os.path.normpath(content_dir)
             content_dir_str = str(content_dir)
             disk_level_dir = content_dir_str.split("OriginalFiles")[0]
@@ -34,5 +35,6 @@ for item_level_dir in os.listdir(working_dir):
             output_file_name = os.path.normpath(output_file_name)
             os.mkdir(migratedfiles_dir)
             subprocess.run(["C:\Program Files\HandBrake\HandBrakeCLI.exe", "-i", content_dir, "-o", output_file_name, "-e", "x264", "-q", "20", "-B", "160", "--comb-detect=fast", "-5"])
+            log = open("HandBrakeConversion.log", 'a')
             print("Converted video file created at", output_file_name, '\n', file=log)
             log.close()
